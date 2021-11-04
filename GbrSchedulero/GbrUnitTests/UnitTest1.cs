1a68eb30445e063798502edf7d383646b0b2971e
fisherdog1 <aaronlinnell@gmail.com>
2021-11-04 17:30:11 -0500

    user secrets

--- GbrSchedulero/GbrUnitTests/UnitTest1.cs
+++ GbrSchedulero/GbrUnitTests/UnitTest1.cs
@@ -1,11 +1,24 @@
 using GbrSchedulero;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Extensions.Configuration;
+using System.Configuration;
 
 namespace GbrUnitTests
 {
     [TestClass]
     public class UnitTest1
     {
+        public IConfigurationRoot? Configuration;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            IConfigurationBuilder config = new ConfigurationBuilder()
+                .AddUserSecrets<UnitTest1>();
+
+            this.Configuration = config.Build();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
@@ -16,6 +29,15 @@ namespace GbrUnitTests
         public void DatabaseConnTest()
         {
             string user = System.Environment.GetEnvironmentVariable("DB_SECRET");
+
+            if (string.IsNullOrEmpty(user))
+            {
+                user = Configuration["DB_SECRET"];
+
+                if (string.IsNullOrEmpty(user))
+                    throw new System.Exception("DB_SECRET not found!");
+            }
+
             int result = Program.DatabaseConnTest(user);
             Assert.AreEqual(result, 1);
         }
