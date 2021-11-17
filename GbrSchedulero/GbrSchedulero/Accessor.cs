f0e63d55675cda34c53b2335c626c6e12748a4af
BrianABelt <brianbelt01@gmail.com>
2021-11-17 17:48:32 -0600

    Added base accessor class, must be expanded

---
+++ GbrSchedulero/GbrSchedulero/Accessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GbrSchedulero
+{
+    //Used to take any data needed from database
+    class Accessor
+    {
+
+    }
+}
