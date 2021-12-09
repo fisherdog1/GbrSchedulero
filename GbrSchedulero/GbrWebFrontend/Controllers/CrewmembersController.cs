544073e31ad32d82714f8f3435b137ee268ece4c
Nhien Nguyen <nnguyen102@huskers.unl.edu>
2021-12-09 13:03:03 -0600

    ui

---
+++ GbrSchedulero/GbrWebFrontend/Controllers/CrewmembersController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CHA.Data;
+using GbrSchedulero;
+
+namespace GbrWebFrontend.Controllers
+{
+    public class CrewmembersController : Controller
+    {
+        private readonly FlightScheduleDbContext _context;
+
+        public CrewmembersController(FlightScheduleDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Crewmembers
+        public async Task<IActionResult> Index()
+        {
+            var Crewmembers = _context.Crewmembers.Include(a => a.Flights).AsNoTracking();
+            return View(await _context.Crewmembers.ToListAsync());
+        }
+
+        // GET: Crewmembers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var crewmember = await _context.Crewmembers
+            .FirstOrDefaultAsync(m => m.CrewmemberID == id);
+            //var crewmember = await _context.Crewmembers.Include(a => a.Flights).ThenInclude(b => b.Crewmember).SingleOrDefaultAsync(c => c.CrewmemberID == id);
+            if (crewmember == null)
+            {
+                return NotFound();
+            }
+
+            return View(crewmember);
+        }
+
+        // GET: Crewmembers/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Crewmembers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CrewmemberID,FirstName,LastName")] Crewmember crewmember)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(crewmember);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(crewmember);
+        }
+
+        // GET: Crewmembers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var crewmember = await _context.Crewmembers.FindAsync(id);
+            if (crewmember == null)
+            {
+                return NotFound();
+            }
+            return View(crewmember);
+        }
+
+        // POST: Crewmembers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CrewmemberID,FirstName,LastName")] Crewmember crewmember)
+        {
+            if (id != crewmember.CrewmemberID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(crewmember);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CrewmemberExists(crewmember.CrewmemberID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(crewmember);
+        }
+
+        // GET: Crewmembers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var crewmember = await _context.Crewmembers
+                .FirstOrDefaultAsync(m => m.CrewmemberID == id);
+            if (crewmember == null)
+            {
+                return NotFound();
+            }
+
+            return View(crewmember);
+        }
+
+        // POST: Crewmembers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var crewmember = await _context.Crewmembers.FindAsync(id);
+            _context.Crewmembers.Remove(crewmember);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CrewmemberExists(int id)
+        {
+            return _context.Crewmembers.Any(e => e.CrewmemberID == id);
+        }
+    }
+}
---
+++ GbrSchedulero/GbrWebFrontend/Migrations/20211209180421_Second.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CHA.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChangeOrders_Assignments_CurrentOrderID",
+                table: "ChangeOrders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChangeOrders_Assignments_PreviousOrderID",
+                table: "ChangeOrders");
+
+            migrationBuilder.RenameColumn(
+                name: "PreviousOrderID",
+                table: "ChangeOrders",
+                newName: "PreviousAssignmentID");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrentOrderID",
+                table: "ChangeOrders",
+                newName: "CurrentAssignmentID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ChangeOrders_PreviousOrderID",
+                table: "ChangeOrders",
+                newName: "IX_ChangeOrders_PreviousAssignmentID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ChangeOrders_CurrentOrderID",
+                table: "ChangeOrders",
+                newName: "IX_ChangeOrders_CurrentAssignmentID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChangeOrders_Assignments_CurrentAssignmentID",
+                table: "ChangeOrders",
+                column: "CurrentAssignmentID",
+                principalTable: "Assignments",
+                principalColumn: "FlightCrewAssignmentID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChangeOrders_Assignments_PreviousAssignmentID",
+                table: "ChangeOrders",
+                column: "PreviousAssignmentID",
+                principalTable: "Assignments",
+                principalColumn: "FlightCrewAssignmentID",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChangeOrders_Assignments_CurrentAssignmentID",
+                table: "ChangeOrders");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ChangeOrders_Assignments_PreviousAssignmentID",
+                table: "ChangeOrders");
+
+            migrationBuilder.RenameColumn(
+                name: "PreviousAssignmentID",
+                table: "ChangeOrders",
+                newName: "PreviousOrderID");
+
+            migrationBuilder.RenameColumn(
+                name: "CurrentAssignmentID",
+                table: "ChangeOrders",
+                newName: "CurrentOrderID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ChangeOrders_PreviousAssignmentID",
+                table: "ChangeOrders",
+                newName: "IX_ChangeOrders_PreviousOrderID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ChangeOrders_CurrentAssignmentID",
+                table: "ChangeOrders",
+                newName: "IX_ChangeOrders_CurrentOrderID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChangeOrders_Assignments_CurrentOrderID",
+                table: "ChangeOrders",
+                column: "CurrentOrderID",
+                principalTable: "Assignments",
+                principalColumn: "FlightCrewAssignmentID",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ChangeOrders_Assignments_PreviousOrderID",
+                table: "ChangeOrders",
+                column: "PreviousOrderID",
+                principalTable: "Assignments",
+                principalColumn: "FlightCrewAssignmentID",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
